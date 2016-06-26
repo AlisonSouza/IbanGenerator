d219554386c7ba517ccb19a3ff73103ca4c52445
alisonsouza <alisonrodrigues054@gmail.com>
2016-06-26 20:04:04 -0300

    clean

--- IbanGenerator/src/main/java/com/iban/domain/exceptions/UnsupportedCountryException.java
+++ IbanGenerator/src/main/java/com/iban/domain/exceptions/UnsupportedCountryException.java
@@ -9,52 +9,23 @@ public class UnsupportedCountryException extends IbanGeneratorException {
 
     private String countryCode;
 
-    /**
-     * Constructs a <code>UnsupportedCountryException</code> with no detail message and cause.
-     */
     public UnsupportedCountryException() {
         super();
     }
 
-    /**
-     * Constructs a <code>UnsupportedCountryException</code> with the
-     * specified detail message.
-     *
-     * @param s the detail message.
-     */
     public UnsupportedCountryException(final String s) {
         super(s);
     }
 
-    /**
-     * Constructs a <code>UnsupportedCountryException</code> with the
-     * specified country code and detail message.
-     *
-     * @param countryCode the country code.
-     * @param s the detail message.
-     */
     public UnsupportedCountryException(String countryCode, final String s) {
         super(s);
         this.countryCode = countryCode;
     }
 
-    /**
-     * Constructs a <code>UnsupportedCountryException</code> with the
-     * specified detail message and cause.
-     *
-     * @param s the detail message.
-     * @param t the cause.
-     */
     public UnsupportedCountryException(final String s, final Throwable t) {
         super(s, t);
     }
 
-    /**
-     * Constructs a <code>UnsupportedCountryException</code> with the
-     * specified cause.
-     *
-     * @param t the cause.
-     */
     public UnsupportedCountryException(final Throwable t) {
         super(t);
     }
